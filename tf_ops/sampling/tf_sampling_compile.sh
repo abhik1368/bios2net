a5f07b7af9c3d8dec3866db1b7e36461c83183db
tryptofanik <49122854+tryptofanik@users.noreply.github.com>
2020-03-01 09:10:13 +0100

    Update tf_sampling_compile.sh

--- tf_ops/sampling/tf_sampling_compile.sh
+++ tf_ops/sampling/tf_sampling_compile.sh
@@ -7,7 +7,7 @@ CUDA_INC='/usr/local/cuda/include'
 CUDA_LIB='/usr/local/cuda/lib64/'
 
 # TF1.2
-g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I $TF_INC -I CUDA_INC -lcudart -L CUDA_LIB -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I $TF_INC -I $CUDA_INC -lcudart -L $CUDA_LIB -O2 -D_GLIBCXX_USE_CXX11_ABI=0
 
 # TF1.4
 #g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -I /usr/local/lib/python2.7/dist-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-8.0/lib64/ -L/usr/local/lib/python2.7/dist-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
