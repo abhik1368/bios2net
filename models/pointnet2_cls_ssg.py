11b75f772768b28e77a100df7bd39083f3ba9ebf
tryptofanik <roethel.albert@gmail.com>
2020-04-14 06:58:45 +0200

    Add dropout at the end of pointnets

--- models/pointnet2_cls_ssg.py
+++ models/pointnet2_cls_ssg.py
@@ -41,6 +41,7 @@ def get_model(point_cloud, is_training, n_classes, bn_decay=None):
 
     # Fully connected layers
     net = tf.reshape(l3_points, [batch_size, -1])
+    net = tf_util.dropout(net, keep_prob=0.5, is_training=is_training, scope='dp1') # my change
     net = tf_util.fully_connected(net, 512, bn=True, is_training=is_training, scope='fc1', bn_decay=bn_decay)
     net = tf_util.dropout(net, keep_prob=0.5, is_training=is_training, scope='dp1')
     net = tf_util.fully_connected(net, 256, bn=True, is_training=is_training, scope='fc2', bn_decay=bn_decay)
