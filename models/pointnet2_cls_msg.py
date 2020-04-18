c1a1d546bf74d36d4d76e8821b7e55ff1770ef1d
tryptofanik <roethel.albert@gmail.com>
2020-04-18 08:32:29 +0200

    MSG model takes in multichannel input

--- models/pointnet2_cls_msg.py
+++ models/pointnet2_cls_msg.py
@@ -10,20 +10,25 @@ import numpy as np
 import tf_util
 from pointnet_util import pointnet_sa_module, pointnet_sa_module_msg
 
-def placeholder_inputs(batch_size, num_point):
-    pointclouds_pl = tf.placeholder(tf.float32, shape=(batch_size, num_point, 3))
+def placeholder_inputs(batch_size, num_point, num_channels):
+    pointclouds_pl = tf.placeholder(tf.float32, shape=(batch_size, num_point, num_channels))
     labels_pl = tf.placeholder(tf.int32, shape=(batch_size))
     return pointclouds_pl, labels_pl
 
 
-def get_model(point_cloud, is_training, bn_decay=None):
+def get_model(point_cloud, is_training, n_classes, bn_decay=None):
     """ Classification PointNet, input is BxNx3, output Bx40 """
     batch_size = point_cloud.get_shape()[0].value
     num_point = point_cloud.get_shape()[1].value
     end_points = {}
 
-    l0_xyz = point_cloud
-    l0_points = None
+    if point_cloud.shape[2] > 3:
+        l0_xyz = point_cloud[:, :, :3]
+        l0_points = point_cloud[:, :, 3:]
+    else:
+        l0_xyz = point_cloud
+        l0_points = None
+    end_points['l0_xyz'] = l0_xyz
 
     # Set abstraction layers
     l1_xyz, l1_points = pointnet_sa_module_msg(l0_xyz, l0_points, 512, [0.1,0.2,0.4], [16,32,128], [[32,32,64], [64,64,128], [64,96,128]], is_training, bn_decay, scope='layer1', use_nchw=True)
@@ -36,7 +41,7 @@ def get_model(point_cloud, is_training, bn_decay=None):
     net = tf_util.dropout(net, keep_prob=0.4, is_training=is_training, scope='dp1')
     net = tf_util.fully_connected(net, 256, bn=True, is_training=is_training, scope='fc2', bn_decay=bn_decay)
     net = tf_util.dropout(net, keep_prob=0.4, is_training=is_training, scope='dp2')
-    net = tf_util.fully_connected(net, 40, activation_fn=None, scope='fc3')
+    net = tf_util.fully_connected(net, n_classes, activation_fn=None, scope='fc3')
 
     return net, end_points
 
