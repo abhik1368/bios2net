b37cd67b4bfefee8ef5a61071a1a0add02bfb5c0
tryptofanik <roethel.albert@gmail.com>
2020-04-21 19:04:24 +0200

    Add weight decay to other models

--- models/pointnet2_cls_msg.py
+++ models/pointnet2_cls_msg.py
@@ -16,7 +16,7 @@ def placeholder_inputs(batch_size, num_point, num_channels):
     return pointclouds_pl, labels_pl
 
 
-def get_model(point_cloud, is_training, n_classes, bn_decay=None):
+def get_model(point_cloud, is_training, n_classes, bn_decay=None, weight_decay=None):
     """ Classification PointNet, input is BxNx3, output Bx40 """
     batch_size = point_cloud.get_shape()[0].value
     num_point = point_cloud.get_shape()[1].value
@@ -31,15 +31,15 @@ def get_model(point_cloud, is_training, n_classes, bn_decay=None):
     end_points['l0_xyz'] = l0_xyz
 
     # Set abstraction layers
-    l1_xyz, l1_points = pointnet_sa_module_msg(l0_xyz, l0_points, 512, [0.1,0.2,0.4], [16,32,128], [[32,32,64], [64,64,128], [64,96,128]], is_training, bn_decay, scope='layer1', use_nchw=True)
-    l2_xyz, l2_points = pointnet_sa_module_msg(l1_xyz, l1_points, 128, [0.2,0.4,0.8], [32,64,128], [[64,64,128], [128,128,256], [128,128,256]], is_training, bn_decay, scope='layer2')
-    l3_xyz, l3_points, _ = pointnet_sa_module(l2_xyz, l2_points, npoint=None, radius=None, nsample=None, mlp=[256,512,1024], mlp2=None, group_all=True, is_training=is_training, bn_decay=bn_decay, scope='layer3')
+    l1_xyz, l1_points = pointnet_sa_module_msg(l0_xyz, l0_points, 512, [0.1,0.2,0.4], [16,32,128], [[32,32,64], [64,64,128], [64,96,128]], is_training, bn_decay, scope='layer1', use_nchw=True, weight_decay=weight_decay)
+    l2_xyz, l2_points = pointnet_sa_module_msg(l1_xyz, l1_points, 128, [0.2,0.4,0.8], [32,64,128], [[64,64,128], [128,128,256], [128,128,256]], is_training, bn_decay, scope='layer2', weight_decay=weight_decay)
+    l3_xyz, l3_points, _ = pointnet_sa_module(l2_xyz, l2_points, npoint=None, radius=None, nsample=None, mlp=[256,512,1024], mlp2=None, group_all=True, is_training=is_training, bn_decay=bn_decay, scope='layer3', weight_decay=weight_decay)
 
     # Fully connected layers
     net = tf.reshape(l3_points, [batch_size, -1])
-    net = tf_util.fully_connected(net, 512, bn=True, is_training=is_training, scope='fc1', bn_decay=bn_decay)
+    net = tf_util.fully_connected(net, 512, bn=True, is_training=is_training, scope='fc1', bn_decay=bn_decay, weight_decay=weight_decay)
     net = tf_util.dropout(net, keep_prob=0.4, is_training=is_training, scope='dp1')
-    net = tf_util.fully_connected(net, 256, bn=True, is_training=is_training, scope='fc2', bn_decay=bn_decay)
+    net = tf_util.fully_connected(net, 256, bn=True, is_training=is_training, scope='fc2', bn_decay=bn_decay, weight_decay=weight_decay)
     net = tf_util.dropout(net, keep_prob=0.4, is_training=is_training, scope='dp2')
     net = tf_util.fully_connected(net, n_classes, activation_fn=None, scope='fc3')
 
--- models/pointnet_cls_basic.py
+++ models/pointnet_cls_basic.py
@@ -19,7 +19,7 @@ def placeholder_inputs(batch_size, num_point, num_channels):
     return pointclouds_pl, labels_pl
 
 
-def get_model(point_cloud, is_training, n_classes, bn_decay=None):
+def get_model(point_cloud, is_training, n_classes, bn_decay=None, weight_decay=None):
     """ Classification PointNet, input is BxNx3, output Bx40 """
     batch_size = point_cloud.get_shape()[0].value
     num_point = point_cloud.get_shape()[1].value
@@ -31,23 +31,28 @@ def get_model(point_cloud, is_training, n_classes, bn_decay=None):
     net = tf_util.conv2d(input_image, 64, [1,channels_size],
                          padding='VALID', stride=[1,1],
                          bn=True, is_training=is_training,
-                         scope='conv1', bn_decay=bn_decay)
+                         scope='conv1', bn_decay=bn_decay,
+                         weight_decay=weight_decay)
     net = tf_util.conv2d(net, 64, [1,1],
                          padding='VALID', stride=[1,1],
                          bn=True, is_training=is_training,
-                         scope='conv2', bn_decay=bn_decay)
+                         scope='conv2', bn_decay=bn_decay,
+                         weight_decay=weight_decay)
     net = tf_util.conv2d(net, 64, [1,1],
                          padding='VALID', stride=[1,1],
                          bn=True, is_training=is_training,
-                         scope='conv3', bn_decay=bn_decay)
+                         scope='conv3', bn_decay=bn_decay,
+                         weight_decay=weight_decay)
     net = tf_util.conv2d(net, 128, [1,1],
                          padding='VALID', stride=[1,1],
                          bn=True, is_training=is_training,
-                         scope='conv4', bn_decay=bn_decay)
+                         scope='conv4', bn_decay=bn_decay,
+                         weight_decay=weight_decay)
     net = tf_util.conv2d(net, 1024, [1,1],
                          padding='VALID', stride=[1,1],
                          bn=True, is_training=is_training,
-                         scope='conv5', bn_decay=bn_decay)
+                         scope='conv5', bn_decay=bn_decay,
+                         weight_decay=weight_decay)
 
     # Symmetric function: max pooling
     net = tf_util.max_pool2d(net, [num_point,1],
@@ -56,12 +61,12 @@ def get_model(point_cloud, is_training, n_classes, bn_decay=None):
     # MLP on global point cloud vector
     net = tf.reshape(net, [batch_size, -1])
     net = tf_util.fully_connected(net, 512, bn=True, is_training=is_training,
-                                  scope='fc1', bn_decay=bn_decay)
+                                  scope='fc1', bn_decay=bn_decay, weight_decay=weight_decay)
     net = tf_util.fully_connected(net, 256, bn=True, is_training=is_training,
-                                  scope='fc2', bn_decay=bn_decay)
+                                  scope='fc2', bn_decay=bn_decay, weight_decay=weight_decay)
     net = tf_util.dropout(net, keep_prob=0.7, is_training=is_training,
                           scope='dp1')
-    net = tf_util.fully_connected(net, n_classes, activation_fn=None, scope='fc3')
+    net = tf_util.fully_connected(net, n_classes, activation_fn=None, scope='fc3', weight_decay=weight_decay)
 
     return net, end_points
 
