259fa04c2e7ff8cec4b8dbe4ce48e8b8e414c095
tryptofanik <roethel.albert@gmail.com>
2020-05-09 14:50:10 +0200

    Merge structural and sequence networks

--- models/pointnet2_cls_ssg_conv.py
+++ models/pointnet2_cls_ssg_conv.py
@@ -19,7 +19,7 @@ def placeholder_inputs(batch_size, num_point, num_channels):
     labels_pl = tf.placeholder(tf.int32, shape=(batch_size))
     return pointclouds_pl, labels_pl
 
-def get_model(point_cloud, is_training, n_classes, bn_decay=None, weight_decay=None):
+def get_model(point_cloud, is_training, n_classes, bn_decay=None, weight_decay=None, **kwargs):
     """ Classification PointNet, input is BxNx3, output Bx40 """
     batch_size = point_cloud.get_shape()[0].value
     num_point = point_cloud.get_shape()[1].value
@@ -33,55 +33,77 @@ def get_model(point_cloud, is_training, n_classes, bn_decay=None, weight_decay=N
         l0_points = None
     end_points['l0_xyz'] = l0_xyz
 
-    input_image = tf.expand_dims(point_cloud, -1)
-    net = tf_util.conv2d(input_image, 64, [1,channels_size],
-                         padding='VALID', stride=[1,1],
-                         bn=True, is_training=is_training,
-                         scope='conv1', bn_decay=bn_decay,
-                         weight_decay=weight_decay)
-    net = tf_util.conv2d(net, 64, [1,1],
-                         padding='VALID', stride=[1,1],
-                         bn=True, is_training=is_training,
-                         scope='conv2', bn_decay=bn_decay,
-                         weight_decay=weight_decay)
-    net = tf_util.conv2d(net, 64, [1,1],
-                         padding='VALID', stride=[1,1],
-                         bn=True, is_training=is_training,
-                         scope='conv3', bn_decay=bn_decay,
-                         weight_decay=weight_decay)
-    net = tf_util.conv2d(net, 128, [1,1],
-                         padding='VALID', stride=[1,1],
-                         bn=True, is_training=is_training,
-                         scope='conv4', bn_decay=bn_decay,
-                         weight_decay=weight_decay)
-    net = tf_util.conv2d(net, 1024, [1,1],
-                         padding='VALID', stride=[1,1],
-                         bn=True, is_training=is_training,
-                         scope='conv5', bn_decay=bn_decay,
-                         weight_decay=weight_decay)
+    input_image = tf.expand_dims(point_cloud, 2)
 
-    # Symmetric function: max pooling
-    net = tf_util.max_pool2d(net, [num_point,1],
-                             padding='VALID', scope='maxpool')
+    net, conv_ker = tf_util.inception(input_image, 64, scope='seq_conv1',
+                            kernel_heights=[1, 3, 5, 7], kernel_widths=[1, 1, 1, 1],
+                            kernels_fraction=[2, 2, 2, 2],
+                            return_kernel=True,
+                            weight_decay=weight_decay, bn=True, bn_decay=bn_decay,
+                            is_training=is_training)
+    end_points['conv_ker'] = conv_ker
+    net = tf_util.max_pool2d(net, kernel_size=[2, 1], scope='seq_maxpool_1',
+                            stride=[2,1], padding='SAME')
+    net = tf_util.inception(net, 64, scope='seq_conv2',
+                            kernel_heights=[1, 3, 5, 7], kernel_widths=[1, 1, 1, 1],
+                            kernels_fraction=[3, 2, 2, 1],
+                            weight_decay=weight_decay, bn=True, bn_decay=bn_decay,
+                            is_training=is_training)
+    net = tf_util.max_pool2d(net, kernel_size=[2, 1], scope='seq_maxpool_2',
+                            stride=[2,1], padding='SAME')
+    net = tf_util.inception(net, 128, scope='seq_conv3',
+                            kernel_heights=[1, 3, 5, 7], kernel_widths=[1, 1, 1, 1],
+                            kernels_fraction=[3, 2, 2, 1],
+                            weight_decay=weight_decay, bn=True, bn_decay=bn_decay,
+                            is_training=is_training)
+    net = tf_util.max_pool2d(net, kernel_size=[2, 1], scope='seq_maxpool_3',
+                            stride=[2,1], padding='SAME')
+    net = tf_util.inception(net, 256, scope='seq_conv4',
+                            kernel_heights=[1, 3, 5, 7], kernel_widths=[1, 1, 1, 1],
+                            kernels_fraction=[3, 2, 2, 1],
+                            weight_decay=weight_decay, bn=True, bn_decay=bn_decay,
+                            is_training=is_training)
+    net = tf_util.max_pool2d(net, kernel_size=[2, 1], scope='seq_maxpool_4',
+                            stride=[2,1], padding='SAME')
+    net = tf_util.inception(net, 512, scope='seq_conv5',
+                            kernel_heights=[1, 3, 5, 7], kernel_widths=[1, 1, 1, 1],
+                            kernels_fraction=[3, 2, 2, 1],
+                            weight_decay=weight_decay, bn=True, bn_decay=bn_decay,
+                            is_training=is_training)
+    net = tf_util.max_pool2d(net, kernel_size=[2, 1], scope='seq_maxpool_5',
+                            stride=[2,1], padding='SAME')
+    net = tf_util.inception(net, 1024, scope='seq_conv6',
+                            kernel_heights=[1, 3, 5, 7], kernel_widths=[1, 1, 1, 1],
+                            kernels_fraction=[3, 2, 2, 1],
+                            weight_decay=weight_decay, bn=True, bn_decay=bn_decay,
+                            is_training=is_training)
 
-    conv_net = tf.reshape(net, [batch_size, -1])
+    net = tf_util.avg_pool2d(net, kernel_size=[32, 1], scope='seq_global_avg_pool', stride=[32,1], padding='SAME')
+
+    # # Symmetric function: max pooling
+    # net = tf_util.max_pool2d(net, [num_point,1],
+    #                          padding='VALID', scope='maxpool')
+
+    conv_net = tf.squeeze(net)
 
     # Set abstraction layers
     # Note: When using NCHW for layer 2, we see increased GPU memory usage (in TF1.4).
     # So we only use NCHW for layer 1 until this issue can be resolved.
-    l1_xyz, l1_points, l1_indices = pointnet_sa_module(l0_xyz, l0_points, npoint=512, radius=0.2, nsample=32, mlp=[64,64,128], mlp2=None, group_all=False, is_training=is_training, bn_decay=bn_decay, scope='layer1', weight_decay=weight_decay, use_nchw=True)
-    l2_xyz, l2_points, l2_indices = pointnet_sa_module(l1_xyz, l1_points, npoint=128, radius=0.4, nsample=64, mlp=[128,128,256], mlp2=None, group_all=False, is_training=is_training, bn_decay=bn_decay, scope='layer2', weight_decay=weight_decay)
-    l3_xyz, l3_points, l3_indices = pointnet_sa_module(l2_xyz, l2_points, npoint=None, radius=None, nsample=None, mlp=[256,512,1024], mlp2=None, group_all=True, is_training=is_training, bn_decay=bn_decay, scope='layer3', weight_decay=weight_decay)
+    l1_xyz, l1_points, l1_indices, pt_ker = pointnet_sa_module(l0_xyz, l0_points, npoint=512, radius=0.2, nsample=32, mlp=[64,64,128], mlp2=None, group_all=False, is_training=is_training, bn_decay=bn_decay, scope='layer1', weight_decay=weight_decay, use_nchw=True)
+    end_points['pt_ker'] = pt_ker
+    l2_xyz, l2_points, l2_indices, _ = pointnet_sa_module(l1_xyz, l1_points, npoint=128, radius=0.4, nsample=64, mlp=[128,128,256], mlp2=None, group_all=False, is_training=is_training, bn_decay=bn_decay, scope='layer2', weight_decay=weight_decay)
+    l3_xyz, l3_points, l3_indices, _ = pointnet_sa_module(l2_xyz, l2_points, npoint=None, radius=None, nsample=None, mlp=[256,512,1024], mlp2=None, group_all=True, is_training=is_training, bn_decay=bn_decay, scope='layer3', weight_decay=weight_decay)
     net = tf.reshape(l3_points, [batch_size, -1])
 
     net = tf.concat([conv_net, net], axis=-1)
+    end_points['feature_vector'] = net
 
     # Fully connected layers
-    net = tf_util.dropout(net, keep_prob=0.5, is_training=is_training, scope='dp1') # my change
+    net = tf_util.dropout(net, rate=0.5, is_training=is_training, scope='dp1') # my change
     net = tf_util.fully_connected(net, 512, bn=True, is_training=is_training, scope='fc1', bn_decay=bn_decay, weight_decay=weight_decay)
-    net = tf_util.dropout(net, keep_prob=0.5, is_training=is_training, scope='dp1')
+    net = tf_util.dropout(net, rate=0.5, is_training=is_training, scope='dp1')
     net = tf_util.fully_connected(net, 256, bn=True, is_training=is_training, scope='fc2', bn_decay=bn_decay, weight_decay=weight_decay)
-    net = tf_util.dropout(net, keep_prob=0.5, is_training=is_training, scope='dp2')
+    net = tf_util.dropout(net, rate=0.5, is_training=is_training, scope='dp2')
     net = tf_util.fully_connected(net, n_classes, activation_fn=None, scope='fc3')
 
     return net, end_points
