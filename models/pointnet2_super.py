5e0e8bf2d8c2ad18b873421750df91149c3cb0ff
Albert Roethel <roethel.albert@gmail.com>
2020-05-24 09:24:24 +0200

    Cleanup in main model

--- models/pointnet2_super.py
+++ models/pointnet2_super.py
@@ -55,8 +55,37 @@ def conv_network(point_cloud, is_training, scope, channels_out=1024, bn_decay=No
     return net, kernel
 
 
-def get_model(point_cloud, is_training, n_classes, bn_decay=None, weight_decay=None, extractor=True, **kwargs):
+
+def classification_head(net, is_training, scope, n_classes, bn_decay, weight_decay):
+    net = tf_util.dropout(net, rate=0.6, is_training=is_training, scope=scope+'_dp1') # my change
+    net = tf_util.fully_connected(net, 512, bn=True, is_training=is_training, scope=scope+'_fc1', bn_decay=bn_decay, 
+                                  weight_decay=weight_decay)
+    net = tf_util.dropout(net, rate=0.6, is_training=is_training, scope=scope+'_dp2')
+    net = tf_util.fully_connected(net, 256, bn=True, is_training=is_training, scope=scope+'_fc2', bn_decay=bn_decay, 
+                                  weight_decay=weight_decay)
+    net = tf_util.dropout(net, rate=0.6, is_training=is_training, scope=scope+'_dp3')
+    net = tf_util.fully_connected(net, n_classes, activation_fn=None, scope=scope+'_fc3')
+    return net
+
+def extractor_layers(point_cloud, n, is_training, bn_decay, bn=True, n_out=32, scope='extractor'):
+    input_image = tf.expand_dims(point_cloud, 2)
+    
+    for i in range(n):
+        net, conv_ker = tf_util.inception(input_image, n_out, scope=f'seq_conv{i}',
+                            kernel_heights=[1, 3, 5, 7], kernel_widths=[1, 1, 1, 1],
+                            kernels_fraction=[2, 2, 2, 2],
+                            return_kernel=True,
+                            bn=bn, bn_decay=bn_decay,
+                            is_training=is_training)
+        if i == 0:
+            ker = conv_ker
+
+    return tf.squeeze(net), ker
+
+def get_model(point_cloud, is_training, n_classes, bn_decay=None, weight_decay=None, 
+              extractor=True, temporal=True, **kwargs):
     """ Classification PointNet, input is BxNx3, output Bx40 """
+    
     batch_size = point_cloud.get_shape()[0].value
     num_point = point_cloud.get_shape()[1].value
     channels_size = point_cloud.get_shape()[2].value
@@ -67,98 +96,90 @@ def get_model(point_cloud, is_training, n_classes, bn_decay=None, weight_decay=N
     else:
         l0_xyz = point_cloud
         l0_points = None
-    end_points['l0_xyz'] = l0_xyz
+    end_points['input_point_cloud'] = point_cloud
+    
+    if extractor:
+        print('==== USING EXTRACTOR ====')
+        extr, ker = extractor_layers(point_cloud, 5, is_training, bn_decay, bn=True, n_out=32)
+        end_points['extr'] = extr
+        end_points['extr_ker'] = ker
+        l0_points = tf.concat([l0_points, extr], axis=-1)
 
-#     if extractor:
-#         input_image = tf.expand_dims(point_cloud, 2)
-
-#         net, conv_ker = tf_util.inception(input_image, 64, scope='seq_conv1',
-#                                 kernel_heights=[1, 3, 5, 7], kernel_widths=[1, 1, 1, 1],
-#                                 kernels_fraction=[2, 2, 2, 2],
-#                                 return_kernel=True,
-#                                 bn=True, bn_decay=bn_decay,
-#                                 is_training=is_training)
-#         end_points['conv_ker'] = conv_ker
-#         net, conv_ker = tf_util.inception(input_image, 32, scope='seq_conv2',
-#                                 kernel_heights=[1, 3, 5, 7], kernel_widths=[1, 1, 1, 1],
-#                                 kernels_fraction=[2, 2, 2, 2],
-#                                 return_kernel=True,
-#                                 bn=True, bn_decay=bn_decay,
-#                                 is_training=is_training)
-
-#         conv_net = tf.squeeze(net)
-
-#         l0_points_conv = tf.concat([l0_points, conv_net], axis=-1)
-    l0_points_conv = l0_points
-    end_points['points'] = l0_points_conv
-
-
+    # first pointnet layer
     l1_xyz, l1_points, l1_indices, pt_ker = pointnet_sa_module(
-        l0_xyz, l0_points_conv, npoint=512, radius=0.2, nsample=64, mlp=[64,64,128], 
+        l0_xyz, l0_points, npoint=512, radius=0.2, nsample=64, mlp=[64,64,128], 
         mlp2=None, group_all=False, is_training=is_training, bn_decay=bn_decay, 
         scope='layer1', use_nchw=True) 
     end_points['pt_ker'] = pt_ker
     
-    net = tf.concat([l1_xyz, l0_points[:, ::(l0_points.shape[1] // 512), :], l1_points], axis=-1)
-    end_points['temp1_input'] = net
-    temporal_1, ker = conv_network(net, is_training=is_training, scope='temp1', channels_out=1526, bn_decay=bn_decay)
-    end_points['temp1_ker'] = ker
-    
+    # second pointnet layer
     l2_xyz, l2_points, l2_indices, _ = pointnet_sa_module(
         l1_xyz, l1_points, npoint=128, radius=0.4, nsample=128, mlp=[128,128,256], 
         mlp2=None, group_all=False, is_training=is_training, bn_decay=bn_decay, 
         scope='layer2')
     
-#     end_points['l2_xyz'] = l2_xyz
-#     net = tf.concat([l2_xyz, l0_points[:, ::(l0_points.shape[1] // 128), :], l2_points], axis=-1)
-#     end_points['temp2_input'] = net
-#     temporal_2 = conv_network(net, is_training=is_training, scope='temp2', channels_out=1024, bn_decay=bn_decay)
-    
-    
+    # third potinent layer
     l3_xyz, l3_points, l3_indices, _ = pointnet_sa_module(
         l2_xyz, l2_points, npoint=None, radius=None, nsample=None, mlp=[256,512,1024], 
         mlp2=None, group_all=True, is_training=is_training, bn_decay=bn_decay, 
         scope='layer3')
     ptnet_out = tf.reshape(l3_points, [batch_size, -1])
-    
-    
-    
-    net = tf_util.dropout(ptnet_out, rate=0.6, is_training=is_training, scope='pt_dp1') # my change
-    net = tf_util.fully_connected(net, 512, bn=True, is_training=is_training, scope='pt_fc1', bn_decay=bn_decay, weight_decay=weight_decay)
-    net = tf_util.dropout(net, rate=0.6, is_training=is_training, scope='pt_dp1')
-    net = tf_util.fully_connected(net, 256, bn=True, is_training=is_training, scope='pt_fc2', bn_decay=bn_decay, weight_decay=weight_decay)
-    net = tf_util.dropout(net, rate=0.6, is_training=is_training, scope='pt_dp2')
-    pt_pred = tf_util.fully_connected(net, n_classes, activation_fn=None, scope='pt_fc3')
+        
+    if temporal:
+        print('==== USING TEMPORAL CONVOLUTION ====')
+        # first temporal
+        net = tf.concat([l1_xyz, l0_points[:, ::(num_point // 512), :], l1_points], axis=-1)
+        end_points['temp1_input'] = net
+        temporal_1, ker = conv_network(net, is_training=is_training, scope='temp1', channels_out=1024, bn_decay=bn_decay)
+        end_points['temp1_ker'] = ker
+
+        # second temporal
+        net = tf.concat([l2_xyz, l0_points[:, ::(l0_points.shape[1] // 128), :], l2_points], axis=-1)
+        end_points['temp2_input'] = net
+        temporal_2, ker = conv_network(net, is_training=is_training, scope='temp2', channels_out=1024, bn_decay=bn_decay)
+
+        # classification for temporal
+        temp1_pred = classification_head(temporal_1, is_training, 'cls_temp1', n_classes, bn_decay, weight_decay)
+        temp2_pred = classification_head(temporal_2, is_training, 'cls_temp2', n_classes, bn_decay, weight_decay)
+
+    # classification from potinent
+    pt_pred = classification_head(ptnet_out, is_training, 'cls_ptnet', n_classes, bn_decay, weight_decay)
 
     
-    
-    net = tf.concat([ptnet_out, temporal_1], axis=-1)
+    # concat output from poitnet and temporal networks
+    if temporal:
+#         net = tf.concat([ptnet_out, temporal_1, temporal_2], axis=-1)
+        net = tf.math.add_n([ptnet_out, temporal_1, temporal_2])
+        print(net)
+    else:
+        net = ptnet_out
     end_points['feature_vector'] = net
 
-    # Fully connected layers
-    net = tf_util.dropout(net, rate=0.6, is_training=is_training, scope='dp1') # my change
-    net = tf_util.fully_connected(net, 512, bn=True, is_training=is_training, scope='fc1', bn_decay=bn_decay, weight_decay=weight_decay)
-    net = tf_util.dropout(net, rate=0.6, is_training=is_training, scope='dp1')
-    net = tf_util.fully_connected(net, 256, bn=True, is_training=is_training, scope='fc2', bn_decay=bn_decay, weight_decay=weight_decay)
-    net = tf_util.dropout(net, rate=0.6, is_training=is_training, scope='dp2')
-    net = tf_util.fully_connected(net, n_classes, activation_fn=None, scope='fc3')
-
-    return pt_pred, net, end_points
+    # classification from entire network
+    final_pred = classification_head(net, is_training, 'cls_net', n_classes, bn_decay, weight_decay)
+    if temporal:
+        return [temp1_pred, temp2_pred, pt_pred], final_pred, end_points
+    else:
+        return [pt_pred], final_pred, end_points
 
 
-def get_loss(pt_pred, pred, label, end_points):
+def get_loss(site_preds, pred, label, end_points):
     """ pred: B*NUM_CLASSES,
         label: B, """
+    
     loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=pred, labels=label)
-    loss2 = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=pt_pred, labels=label)
-    classify_loss = tf.reduce_mean(loss)
-    classify_loss2 = tf.reduce_mean(loss2)
+    classify_loss = tf.reduce_mean(loss) * 0.4
     tf.summary.scalar('classify loss', classify_loss)
     tf.add_to_collection('losses', classify_loss)
-    tf.summary.scalar('classify loss', classify_loss2)
-    tf.add_to_collection('losses', classify_loss2)
 
-    return classify_loss, classify_loss2
+    classify_aux_losses = []
+    for i in site_preds:
+        loss2 = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=i, labels=label)
+        classify_loss2 = tf.reduce_mean(loss2) * 0.2 # (1 - 0.4) / len(site_preds)
+        classify_aux_losses.append(tf.reduce_mean(loss2))
+        tf.summary.scalar('classify loss', classify_loss2)
+        tf.add_to_collection('losses', classify_loss2)
+    return classify_loss, classify_aux_losses
 
 
 if __name__=='__main__':
